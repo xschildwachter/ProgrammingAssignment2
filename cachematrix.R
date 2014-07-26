01454f62af4b959b56e213915d50d1fced4337da
xschildwachter <xschildwachter@gmail.com>
2014-07-26 16:09:45 -0700

    Base

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Returns a cached version of the matrix passed as argument
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(solve) s <<- solve
+  getsolved <- function() s
+  list(set=set, get=get, setsolved=setsolved, getsolved=getsolved)
 }
 
-
-## Write a short comment describing this function
-
+## Returns the inverse of the cached matrix passed as argument
+## Returns cached value if available, computes otherwise
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolved()
+  if (! is.null(s)) {
+    message('Getting cached data')
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolved(s)
+  s
 }
